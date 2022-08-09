eba8eb902a0fe91cecf07f2b3daa9ef732713f42
wfh <3274597275@qq.com>
2022-08-09 15:38:05 +0800

    add README

---
+++ ExportAPI/src/main/java/org/apache/dubbo/Interface/ServiceConfigInterface.java
@@ -0,0 +1,21 @@
+package org.apache.dubbo.Interface;
+
+import org.apache.dubbo.config.ApplicationConfig;
+import org.apache.dubbo.config.MetadataReportConfig;
+import org.apache.dubbo.config.RegistryConfig;
+
+/**
+ * //TODO add interface commment here
+ *
+ * @Author wfh
+ * @Date 2022/8/9 上午11:29
+ */
+public interface ServiceConfigInterface {
+    void setInterface(Class<?> klass);
+    void setRef(Object object);
+    void setApplication(ApplicationConfig application);
+    void setRegistry(RegistryConfig registry);
+    void setMetadataReportConfig(MetadataReportConfig metadataReportConfig);
+    void export();
+}
+// 等会不行把参数名字该全
